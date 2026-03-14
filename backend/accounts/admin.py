5003a1967483e0bf677c1c60ed6707fe3b74de5a
RaghavRaval <raghavshayar3507@gmail.com>
2026-03-14 17:01:45 +0530

    Splash Screen added

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
 from .models import UserCustom, Profile
 
-<<<<<<< HEAD
-@admin.register(UserCustom)
-class UserCustomAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'created_at')
-    search_fields = ('username', 'email')
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'age', 'fitness_goal')
-    search_fields = ('user__username', 'fitness_goal')
-=======
 # Register your models here.
 
 from .models import UserCustom
 
 admin.site.register(UserCustom)
->>>>>>> 5ff3483 (Splash Screen added)
